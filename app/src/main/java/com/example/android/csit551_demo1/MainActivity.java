8e97ce849a3a03190c512b50c77bb2d5d0ccde5e
priyankabp <pbphapale@gmail.com>
2017-01-24 14:46:52 -0500

    Change color of button on click

--- app/src/main/java/com/example/android/csit551_demo1/MainActivity.java
+++ app/src/main/java/com/example/android/csit551_demo1/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.csit551_demo1;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +17,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public int getRandomColor(){
+        Random rnd = new Random();
+        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+    }
+
     public void mOnClick(View view) {
         ((Button)view).setText("CLICKED " + counter++);
+         view.setBackgroundColor(getRandomColor());
     }
 }
