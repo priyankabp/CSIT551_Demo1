2260f355b24d946b9c997842c951260eb46ebd3f
priyankabp <pbphapale@gmail.com>
2017-01-24 14:11:59 -0500

    Added Counter to button

--- app/src/main/java/com/example/android/csit551_demo1/MainActivity.java
+++ app/src/main/java/com/example/android/csit551_demo1/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.android.csit551_demo1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    int counter=1;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mOnClick(View view) {
+        ((Button)view).setText("CLICKED " + counter++);
     }
 }
